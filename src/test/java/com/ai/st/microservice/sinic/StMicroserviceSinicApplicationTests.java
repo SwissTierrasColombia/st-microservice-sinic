fc1bb8eef8e9a0301028291174a462f62c2a3c71
shiido <jhonfr29@gmail.com>
2021-11-17 12:27:08 -0500

    feat: Setup Microservice

---
+++ src/test/java/com/ai/st/microservice/sinic/StMicroserviceSinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.st.microservice.sinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StMicroserviceSinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
